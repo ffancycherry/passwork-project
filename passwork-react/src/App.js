f47b1c76b426cd183cac561183ea1dd08958e24b
ffancycherry <kriskisylz@gmail.com>
2023-05-25 12:40:35 +0700

    feat: transferring a layout

--- passwork-react/src/App.js
+++ passwork-react/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./frontend/main_page.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      <section className="wrapper">
+        
+          <div className="directory">
+              <input type="text" placeholder="Найти"/>
+              <hr/>
+              <h2>ОРГАНИЗАЦИЯ</h2>
+              <hr/>
+              <h2>ЛИЧНОЕ</h2>
+          </div>
+         
+          <div className="preview">
+              
+              <div className="navigation">  
+                  <div>
+                    <h2>Home</h2>
+                    <h2>Components</h2>
+                    <h2>Breadcrumb</h2>
+                  </div> 
+                  <button>Добавить пароль</button>
+              </div>
+              <div className="passwords">
+                  
+                  <div className="passwords_name">
+                      <h2>ПАРОЛИ</h2>
+                  </div>
+                
+                  <div className="passwords_info">
+                      <div className="passwords_info_idt">
+                      <h1>Name profile</h1>
+                      <button></button>
+                      </div>
+                      <label>Логин</label>
+                      <input type="text"/>
+                      <hr/>
+                      <label>Пароль</label>
+                      <input type="password"/>
+                      <hr/>
+                      <label>URL</label>
+                      <input type="url" />
+                      <hr/>                
+                  </div>
+              </div>
+          </div>
+      </section>
     </div>
   );
 }
